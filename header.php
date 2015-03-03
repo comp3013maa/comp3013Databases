b19cd6dd3149d6678fca7a8d2fb2b110d10ace86
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-03 23:12:13 +0000

    Added destroy session after 1 hour

--- header.php
+++ header.php
@@ -1,7 +1,18 @@
 <?php
+ob_start(); // ignore, prevents errors when redirecting 
 
+/* if session not already created, create it */
+if(!isset($_SESSION))  {
+	session_start(); // 
+}  
 
+/* If more than an hour since last activity, destroy session */
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 3600)) {
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+}
 
-
+/*  if session still active - update last activity to current time  */ 
+$_SESSION['LAST_ACTIVITY'] = time(); 
 
 ?>
