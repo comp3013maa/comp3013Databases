59a77781fd8aeece87d59eb154563842437aba97
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-04 22:37:23 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,17 @@
+<html>
 
+<head> 
+	<title> Logged out </title>
+</head>
+
+<body>
+<?php
+  if(isset($_POST['logoutForm'])){ 
+    session_destroy(); 
+    echo 'logged out';  
+    echo 'http://comp3013maa.azurewebsites.net/index.php'; 
+  }
+?>
+
+</body>
+</html>
