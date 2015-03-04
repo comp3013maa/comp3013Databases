0bea2faf2e6a53f03425e40b155ea9cbdefe1a54
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-04 21:57:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
 
 echo '<a href="register.html">Add New User Form</a> <br />' ;
-echo '<a href="login.html">Login</a> <br />' ;
+echo '<a href="login.php">Login</a> <br />' ;
 echo 'lolmmm';
 echo 'Mmm';
 echo 'rufff!';
