d47f9a0c3a2bd03331ed8f37b0753f93adbbbec3
Mahi Khan <mahi.u.khan@googlemail.com>
2015-03-06 21:09:51 +0000

    Removed file

--- notLoggedIn.php-php-require-header.php/notLoggedIn.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "header.php"; //include file - require means must be there or give error, include() is can have it 
-
-echo '<b> You are not currently logged in <b> <br/>';
-echo 'Go back to <a href="index.php">index page</a>;;
-
-require "footer.php";
-?>
