3b3b7e8a435227c98cbc41970372968ac8e58537
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-06 21:15:44 +0000

    Create unauthorised.php

---
+++ unauthorised.php
@@ -0,0 +1,17 @@
+<?php
+require "header.php";
+
+if (!isset($_SESSION['userID'])) {
+  echo '<b> You are not logged in </b> </br>';
+  echo 'Go back to <a href="index.php"> your country</a>';
+
+}
+
+if (isset($_SESSION['userID'])) {
+  echo '<b> You are already logged in </b> </br>';
+  echo 'Go back to <a href="index.php"> your country</a>';  
+}
+
+
+require "footer.php";
+?>
