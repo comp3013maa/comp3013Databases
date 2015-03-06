4f19e0ee5eea3ec7aaf4fa16c1f4c8ff7f7a44ea
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-06 21:03:06 +0000

    File to redirect to when not logged in

---
+++ notLoggedIn.php-php-require-header.php/notLoggedIn.php
@@ -0,0 +1,8 @@
+<?php
+require "header.php"; //include file - require means must be there or give error, include() is can have it 
+
+echo '<b> You are not currently logged in <b> <br/>';
+echo 'Go back to <a href="index.php">index page</a>;;
+
+require "footer.php";
+?>
