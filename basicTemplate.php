430bf9910d8815bbb83c97c61031fe7eac64a81c
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-06 20:52:10 +0000

    Template for a default page using header and footer

--- basicTemplate.php
+++ basicTemplate.php
@@ -1,7 +1,7 @@
 <?php
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
 
-echo 'put code here';
+echo 'put code here to appear in main body of page';
 
 require "footer.php";
 ?>
