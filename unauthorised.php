597bc85b0f0abab011aa8d9f18a2fc0ec4f5a4e8
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-10 22:09:26 +0000

    Update unauthorised.php

--- unauthorised.php
+++ unauthorised.php
@@ -3,7 +3,7 @@ require "header.php";
 
 if (!isset($_SESSION['userID'])) {
   echo '<b> You are not logged in </b> </br>';
-  echo 'Go back to <a href="index.php"> your country</a>';
+  echo 'Go back to <a href="login.php"> your country</a>';
 
 }
 
