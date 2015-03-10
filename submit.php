a992017f6d5a29da9a4434724ed7407720087f2d
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-10 17:44:28 +0000

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,9 +1,12 @@
 <?php
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
+?>
+
 <form action = "submit.php" method = "POST" enctype"multiplart/form-data">
       <input type="file" name="file"> <br><br>
       <input type="submit" value"Submit">
 </form>
+<?php
 require "footer.php";
 ?>
 
