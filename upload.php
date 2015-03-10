94de65db962fdc14b396c0d2632665f6a24f4d25
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-10 22:09:03 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,11 +2,11 @@
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
 
 if (!isset($_SESSION['userID'])) {
-	header('location: login.php?');	 
+	header('location: unauthorised.php?');	 
 	
 }
-echo $name = $_FILES['file']['name'];
-echo $tmp_name = $_FILES['file']['tmp_name'];
+//echo $name = $_FILES['file']['name'];
+//echo $tmp_name = $_FILES['file']['tmp_name'];
 if (isset($name)){
 	if(!empty($name)){
 		
