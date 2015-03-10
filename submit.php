9e480ba75976c5222e4184e2a94661dfef2037b8
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-10 18:07:27 +0000

    Update submit.php

--- submit.php
+++ submit.php
@@ -5,12 +5,15 @@ if (!isset($_SESSION['userID'])) {
 	header('location: unauthorised.php');	 
 }
 
+echo $name = $_FILES['file']['name'];
+
 ?>
 
 <form action = "submit.php" method = "POST" enctype"multiplart/form-data">
       <input type="file" name="file"> <br><br>
       <input type="submit" value"Submit">
 </form>
+
 <?php
 require "footer.php";
 ?>
