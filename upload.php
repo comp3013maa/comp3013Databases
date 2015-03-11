de05010fe1986a187526cdb84472f5a7bed4cc67
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-11 15:22:48 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
-
+/*
 if (!isset($_SESSION['userID'])) {
 	header('location: unauthorised.php?');	 
 	
@@ -20,7 +20,72 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-
+*/
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	 	 // Check for an uploaded file:
+	 	 if (isset($_FILES['upload'])) {
+	 	 	
+	 	 	 // Validate the type. Should be JPEG or PNG.
+	 	 	 $allowed = array ('image/pjpeg',
+'image/jpeg', 'image/JPG', 'image/
+X-PNG', 'image/PNG', 'image/png',
+'image/x-png');
+	 	 	 if (in_array($_FILES['upload']
+['type'], $allowed)) {
+	 	 	
+	 	 	 	 // Move the file over.
+	 	 	 	 if (move_uploaded_file
+($_FILES['upload']['tmp_name'],
+"../uploads/{$_FILES['upload']
+['name']}")) {
+	 	 	 	 	 echo '<p><em>The file has
+been uploaded!</em></p>';
+	 	 	 	 } // End of move... IF.
+	 	 	 	
+	 	 	 } else { // Invalid type.
+	 	 	 	 echo '<p class="error">Please
+upload a JPEG or PNG image.</p>';
+	 	 	 }
+	
+	 	 } // End of isset($_FILES['upload']) IF.
+	 	 
+	 	  // Check for an error:
+	 	 if ($_FILES['upload']['error'] > 0) {
+	 	 	 echo '<p class="error">The file could
+not be uploaded because: <strong>';
+	 	
+	 	 	 // Print a message based upon the
+error.
+	 	 	 switch ($_FILES['upload']['error']) {
+	 	 	 	 case 1:
+	 	 	 	 	 print 'The file exceeds the
+upload_max_filesize setting
+in php.ini.';
+	 	 	 	 	 break;
+	 	 	 	 case 2:
+	 	 	 	 	 print 'The file exceeds the
+MAX_FILE_SIZE setting in the
+HTML form.';
+	 	 	 	 	 break;
+	 	 	 	 case 3:
+	 	 	 	 	 print 'The file was only
+partially uploaded.';
+	 	 	 	 	 break;
+	 	 	 	 case 4:
+	 	 	 	 	 print 'No file was uploaded.';
+	 	 	 	 	 break;
+	 	 	 	 case 6:
+	 	 	 	 	 print 'No temporary folder
+was available.';
+	 	 	 	 	 break;
+	 	 	 	 case 7:
+	 	 	 	 	 print 'Unable to write to
+the disk.';
+	 	 	 	 	 break;
+	 	 	 	 case 8:
+	 	 	 	 	 print 'File upload stopped.';
+	 	 	 	 	 break;
 ?>
 
 <form action = "upload.php" method = "POST" enctype"multiplart/form-data">
