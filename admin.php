789fb759802e18e415ec3f5b4d5366f32166affb
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-12 19:33:48 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -32,7 +32,7 @@ $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791
     <span class="caret"></span>
   </button>
   <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php 	while($row = mysqli_fetch_assoc($result)){ echo $row['firstName'] . $row['lastName'];} ?></a></li>
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php  echo $row['firstName'] . $row['lastName']; ?></a></li>
   </ul>
 </div>
 
