1b916c2af846d3c86736afc27533d27223513247
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-13 15:04:36 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -22,31 +22,7 @@ $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791
 */
 ?>
 
-/*
 
-<div class="dropdown">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php  $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013')
-	 or die('Error' . mysql_error());
-	 
-	$query = 
-	 "SELECT * 
-	  FROM users";  
-	 
-	$result = mysqli_query($connection,$query) or die('Error' . mysql_error());
-	  
-	while($row = mysqli_fetch_assoc($result)){ 
-	echo $row['firstName'];
-	echo $row['lastName'];	
-}	
-	 mysqli_close($connection); ?></a></li>
-  </ul>
-</div>
-*/
 
 <?php
 
@@ -59,14 +35,15 @@ $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791
 	 
 	$result = mysqli_query($connection,$query) or die('Error' . mysql_error());
 	  
-	while($row = mysqli_fetch_assoc($result)){ 
+	
 echo '<div class="dropdown">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
     Dropdown
     <span class="caret"></span>
   </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">'; echo $row['firstName'];
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">';
+  while($row = mysqli_fetch_assoc($result)){ 
+   echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'; echo $row['firstName'];
 	
 echo '	  </a></li>
   </ul>
