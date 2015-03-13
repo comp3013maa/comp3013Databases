cfca101194388bb7c6cf7c78dff80ffdbb68557b
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-13 23:55:26 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,10 +12,13 @@ if (isset ($_POST['uploaded'])){
 
 echo $_FILES['file'];
 	echo 'uploaded';
-}
+
 
 $directory = "ftp://waws-prod-am2-025.ftp.azurewebsites.windows.net/site/wwwroot/uploads/";
-
+if(move_uploaded_file($_FILES['tmp_name'], $directory)) {
+	echo 'looool';
+}
+}
 echo '
 <form action = "upload.php" method = "POST" enctype"multiplart/form-data">
       <input type="file" name="file"> <br><br>
