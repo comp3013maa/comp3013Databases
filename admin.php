7826dfd063774370d21e2b36044bf54ba6d122a7
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-13 15:12:51 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -40,13 +40,14 @@ echo '<div class="dropdown">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
     Dropdown
     <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">';
-  while($row = mysqli_fetch_assoc($result)){ 
-   echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'; echo $row['firstName'];
-	
-echo	'  </a></li>';}
- echo ' </ul>
+  </button>'; while($row = mysqli_fetch_assoc($result)){ 
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+ 
+   echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'; echo $row['firstName']
+	.'</a></li></ul>';
+  	
+  }
+ echo ' 
 </div>';
 
 
