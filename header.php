264e29ca65e94161ff991f6f12c042dad9094db7
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-13 23:26:47 +0000

    Update header.php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ $_SESSION['LAST_ACTIVITY'] = time();
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" id="headerLinks" href="index.php">Proper Abbuz m8</a>
+          <a class="navbar-brand" id="headerLinks" href="index.php">COMP3013</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
