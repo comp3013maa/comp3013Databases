4caac41524bd29135d7b8d294d90893cc69ca51a
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-14 21:48:45 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['userID'])) {
 	header('location: unauthorised.php?');	 
 	
 }
-
+/*
 if (isset ($_POST['uploaded'])){
 var_dump($_FILES['file']);
 $filename = $_FILES['file']['name'];
@@ -23,7 +23,27 @@ if (ftp_put($conn_id, $filename, $temp_name, FTP_ASCII))
 }else{echo 'not uploaded';}
 ftp_close($conn_id);
 }
-
+*/
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["uploaded"])) {
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 echo '
 <form action = "upload.php" method = "POST" enctype = "multipart/form-data">
       <input type="file" name="file" id="file"> <br><br>
