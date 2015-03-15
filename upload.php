80eddfd528ea920f6a588eb5c5d6fe8e8444c484
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-15 01:53:08 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -44,12 +44,12 @@ $extension = pathinfo($directory,PATHINFO_EXTENSION);
 if ($_FILES['file']['size'] > 2000000) {
     echo 'Cannot exceed 2MB';
     $validUpload = 0;
-}/*
-// Allow certain file formats
+}
+
 if($extension != "txt" && $extension != "doc" && $extension != "pdf") {
     echo 'Please ensure file is .txt, .doc, or .pdf';
     $validUpload = 0;
-}
+}/*
 if ($validUpload == 1) {
 	if (move_uploaded_file($_FILES["file"]["tmp_name"], $directory)) {
 		   echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
