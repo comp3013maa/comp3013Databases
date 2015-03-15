474cd392c3f8d9dd12aa5cf66f005a98126addb2
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-15 01:40:25 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -24,47 +24,37 @@ if (ftp_put($conn_id, $filename, $temp_name, FTP_ASCII))
 ftp_close($conn_id);
 }
 */
-if(isset($_POST["uploaded"])) {
-$directory = "uploads/". basename($_FILES["file"]["name"]);
+if(isset($_POST['uploaded'])) {
+$directory = 'uploads/'. basename($_FILES['file']['name']);
 
-$uploadOk = 1;
-$imageFileType = pathinfo($directory,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-/*
-    $check = getimagesize($_FILES["file"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-*/
+$validUpload = true;
+
+$extension = pathinfo($directory,PATHINFO_EXTENSION);
+
+
 // Check if file already exists
 if (file_exists($directory)) {
-    echo "Sorry, file already exists.";
+    
     
 }
 // Check file size
 if ($_FILES["file"]["size"] > 2000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+    echo 'Cannot exceed 2MB';
+    $validUpload = false;
 }
 // Allow certain file formats
-if($imageFileType != "txt" && $imageFileType != "doc" && $imageFileType != "pdf"
-&& $imageFileType != "docx" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+if($extension != "txt" && $extension != "doc" && $extension != "pdf") {
+    echo 'Please ensure file is .txt, .doc, or .pdf';
+    $validUpload = false;
 }
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $directory)) {
-        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+if ($validUpload) {
+	if (move_uploaded_file($_FILES["file"]["tmp_name"], $directory)) {
+		   echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+	} else {
+    		echo 'Upload error';
+      
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo 'File not uploaded';
     }
 }
 	
