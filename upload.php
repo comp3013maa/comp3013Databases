fdbd9b140d541e5eba3cbf430c5421b7d76d2fbf
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-15 01:27:53 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -24,19 +24,50 @@ if (ftp_put($conn_id, $filename, $temp_name, FTP_ASCII))
 ftp_close($conn_id);
 }
 */
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
+if(isset($_POST["submit"])) {
+$directory = "uploads/". basename($_FILES["file"]["name"]);
+
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$imageFileType = pathinfo($directory,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
-if(isset($_POST["uploaded"])) {
+/*
     $check = getimagesize($_FILES["file"]["tmp_name"]);
-   
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+*/
+// Check if file already exists
+if (file_exists($directory)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["file"]["size"] > 2000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "txt" && $imageFileType != "doc" && $imageFileType != "pdf"
+&& $imageFileType != "docx" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $directory)) {
         echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+}
+	
 }
 echo '
 <form action = "upload.php" method = "POST" enctype = "multipart/form-data">
