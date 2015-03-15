8af2f80bb1a2470dcd9c574df1ed87482eb939a8
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-15 13:35:51 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -14,7 +14,7 @@ $directory = 'uploads/'. basename($file);
 $validUpload = true;
 
 $extension = pathinfo($directory,PATHINFO_EXTENSION);
-//echo $directory = 'uploads/'.basename($file,'.'.pathinfo($file)['extension']) . '1.' . $extension;
+
 
 $marker = 0;
 while (file_exists($directory)) {
@@ -23,12 +23,12 @@ while (file_exists($directory)) {
 }//adds a number marker if file exists
 
 if ($_FILES['file']['size'] > 2000000) {
-    echo 'Cannot exceed 2MB';
+    echo 'Cannot exceed 2MB ';
     $validUpload = false;
 }
 
 if($extension != 'txt' && $extension != 'doc' && $extension != 'pdf') {
-    echo 'Please ensure file is .txt, .doc, or .pdf';
+    echo 'Please ensure file is .txt, .doc, or .pdf ';
     $validUpload = false;
 }
 if ($validUpload) {
