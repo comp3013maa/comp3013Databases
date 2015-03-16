c5b2d51abe18e84619fe8821522b5993896df9f9
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-16 01:06:38 +0000

    Update download.php

--- download.php
+++ download.php
@@ -3,9 +3,6 @@ require "header.php"; //include file - require means must be there or give error
 
 
 }
-echo '<form action = "download.php" method = "POST" enctype = "downloadform">
-      <input name="file" value="up.txt" type="hidden"> <br><br>
-      <input type="submit" value="download" name = "download">
-</form>';
+
 require "footer.php";
 ?>
