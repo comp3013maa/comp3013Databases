008c64ea9df1fb4a04a27a4d09d3418e9d392827
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-16 19:07:45 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,56 +3,21 @@
 <?php
 require "header.php"; 
 
-echo '<a href="register.html">Add New User Form</a> <br />' ;
-/*
-$connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013')
-	 or die('Error' . mysql_error());
-	 
-	$query = 
-	 "SELECT * 
-	  FROM users";  
-	 
-	$result = mysqli_query($connection,$query) or die('Error' . mysql_error());
-	  
-	while($row = mysqli_fetch_assoc($result)){ 
-	echo $row['firstName'];
-	echo $row['lastName'];	
-}	
-	 mysqli_close($connection);
-*/
-?>
+if (isset($_GET['add'])) {
+	echo '<a href="register.html">Add New User Form</a> <br />' ;
 
-
-
-<?php
-
- $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013')
-	 or die('Error' . mysql_error());
-	 
-	$query = 
-	 "SELECT * 
-	  FROM users";  
-	 
-	$result = mysqli_query($connection,$query) or die('Error' . mysql_error());
-	  
 	
-echo '<div class="dropdown">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">';
- while($row = mysqli_fetch_assoc($result)){
-   echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'; echo $row['firstName'] . ' ' . $row['lastName'] 
-	.'</a></li>';
-  	
-  }
- echo ' </ul>
-</div>';
+}
+
+if (isset($_GET['browse'])) {
+
+/* DISPLAY USERS */
+
+/* *
 
 
-mysqli_close($connection);
 ?>
+
 
 <?php
 require "footer.php";
