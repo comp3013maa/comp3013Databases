d3f582838b8936b1342d90198c9e54f2d88ac41b
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-16 19:14:03 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,6 +3,17 @@
 <?php
 require "header.php"; 
 
+if (empty($_GET)) {
+	echo '
+	<ul> 
+		<li id = "indexList"> <a href = "admin.php?add" class="listLinks"> Add New User</a> </li>                  		
+		<li id = "indexList"> <a href = "admin.php?browse" class="listLinks">Browse & Edit Users</a> </li>
+		<li id = "indexList"> <a href = "admin.php?allocateGroups" class="listLinks">Allocate Groups </a></li>
+		<li id = "indexList"> <a href = "admin.php?rankings" class="listLinks">Group Rankings </a></li>
+	</ul>   
+	';
+}
+
 if (isset($_GET['add'])) {
 	echo '<a href="register.html">Add New User Form</a> <br />' ;
 
