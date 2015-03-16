7f95257d60908b03e04ec52082e5988a53a0d55d
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-16 01:35:54 +0000

    Update download.php

--- download.php
+++ download.php
@@ -1,8 +1,8 @@
 <?php
 require "header.php"; //include file - require means must be there or give error, include() is can have it 
 
+file_get_contents('uploads/up.txt')
 
-}
 
 require "footer.php";
 ?>
