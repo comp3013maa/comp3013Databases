7aa109dd2c85e437b4c69a9b6c135159982fb2d4
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-16 16:50:11 +0000

    Update report.php

--- report.php
+++ report.php
@@ -5,40 +5,15 @@ echo file_get_contents('uploads/up.txt');
 
 echo '<div class="detailBox">
     <div class="titleBox">
-      <label>Comment Box</label>
-        <button type="button" class="close" aria-hidden="false">&times;</button>
+      <label>Grading Assessments</label>
+        <button type="button" class="close" aria-hidden="true">&times;</button>
     </div>
-    <div class="commentBox">
-        
-        <p class="taskDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-    </div>
+    
     <div class="actionBox">
         <ul class="commentList">
             <li>
-                <div class="commenterImage">
-                  <img src="http://lorempixel.com/50/50/people/6" />
-                </div>
                 <div class="commentText">
-                    <p class="">Hello this is a test comment.</p> <span class="date sub-text">on March 5th, 2014</span>
-
-                </div>
-            </li>
-            <li>
-                <div class="commenterImage">
-                  <img src="http://lorempixel.com/50/50/people/7" />
-                </div>
-                <div class="commentText">
-                    <p class="">Hello this is a test comment and this comment is particularly very long and it goes on and on and on.</p> <span class="date sub-text">on March 5th, 2014</span>
-
-                </div>
-            </li>
-            <li>
-                <div class="commenterImage">
-                  <img src="http://lorempixel.com/50/50/people/9" />
-                </div>
-                <div class="commentText">
-                    <p class="">Hello this is a test comment.</p> <span class="date sub-text">on March 5th, 2014</span>
-
+                    <p class="">Good work!</p> <span class="date sub-text">on March 5th, 2014</span>
                 </div>
             </li>
         </ul>
