3d1b0379dc5576e1d9e8fb73ec1bc069faad97e9
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-17 00:02:23 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,4 @@
-
-
-<?php
-require "header.php"; 
+<?php require "header.php"; 
 
 function displayAssignedTo($groupID) {
 	$connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013') or die('Error' . mysql_error());
