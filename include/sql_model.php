f656d3d431c86e20cbf667a947075a0a507000bd
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-17 19:41:17 +0000

    Update sql_model.php

--- include/sql_model.php
+++ include/sql_model.php
@@ -1,13 +1,53 @@
 <?php
-
 require_once "config.php";
 
 class SQL_Helper {
+
+/*
+* Using prepared statements 
+    $stmt = $dbConnection->prepare('SELECT * FROM employees WHERE name = ?');
+$stmt->bind_param('s', $name);
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()) { }
+*/
+  
+public function __construct() {
+   $this->conn = DbConnect();
+}
+
+public function close() {
+	closeDB($this->conn);
+}
+
     /*
-    *   Constructor
-    *   @params: mysqli - $conn
-    *   @return: none
+    *   Get group allocations list 
+    *   @params: none 
+    *   @return: 
     */
-	public function __construct() {
-        $this->conn = connectDB();
+public function getGroupAllocations() {
+	$stmt = $this->conn->prepare("SELECT groupID
+	FROM groupassignments
+	ORDER BY groupID ASC");
+	// $stmt->bind_param('s', $name);
+
+        if ($stmt->execute()) {
+		$result = $stmt->get_result();
+		$row = array(); $output = "";
+		while ($row = $result->fetch_assoc()) {
+			$output= '<tr> <td>' . htmlentities($groupID) . '</td></tr>';     // . displayAssignedTo($groupID, $connection). '</tr>';
+		}
+		
+		$stmt->free_result();
+            	$stmt->close();
+		return $output; 
+        } 
+        else {
+		die("An error occurred performing the request");
 	}
+}
+
+
+
+
+} // end class	
