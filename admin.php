ada91a587963fe7c1d5c392b8d687364954ff97c
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-17 20:04:03 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -75,7 +75,7 @@ if (isset($_GET['allocateGroups'])) {
       </thead>
       <tbody>
 	<?php
-	echo getGroupAllocations(); 
+	echo $this->sql_model->getGroupAllocations(); 
 	/* 
 	$connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013') or die('Error' . mysql_error());
 	$sql = "SELECT groupID
