df08088b17f1698cd61f8aa67eecc098cef8df1b
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-17 18:09:13 +0000

    Update report.php

--- report.php
+++ report.php
@@ -26,9 +26,9 @@ echo '
         </ul>
        
         <form action="report.php" class="form-inline" method="post" role="form">
+        <input type="text" name="comment" size="300">
             <div class="form-group">
                 <textarea rows="7" cols="80"> Your review </textarea>
-                <input type="text" name="comment" size="300">
             </div>
             <p></p>
             Select Grade
