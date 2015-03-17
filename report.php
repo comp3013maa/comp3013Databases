494add211b7dc31c983f235e81589dee3a5be806
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-17 18:21:40 +0000

    Update report.php

--- report.php
+++ report.php
@@ -32,8 +32,7 @@ echo '
             </div>
             <p></p>
             Select Grade
-        <select>
-        <input name="grade">
+        <select name="grade">
             <option>1</option>
             <option>2</option>
             <option>3</option>
