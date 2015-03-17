f0464e5f058af8998c7f65bd41102635f50f021a
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-17 22:32:06 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,5 @@
 <?php require "header.php"; 
 require_once "include/sql_model.php";
-$sql_model = new SQL_Model();
 
 /*
 function displayAssignedTo($groupID, $connection) {
@@ -77,7 +76,9 @@ if (isset($_GET['allocateGroups'])) {
       </thead>
       <tbody>
 	<?php
+	$sql_model = new SQL_Model();
 	echo $sql_model->getGroupAllocations(); 
+	$sql_model->close();
 	/* 
 	$connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013') or die('Error' . mysql_error());
 	$sql = "SELECT groupID
