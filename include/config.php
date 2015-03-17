94ad71a53fc1dbd97214c40269f95f8925077c38
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-17 20:32:47 +0000

    Update config.php

--- include/config.php
+++ include/config.php
@@ -8,17 +8,17 @@ function DbConnect () {
 	
 	$connection = mysqli_connect($host, $user, $pass, $db);
 	// or die("Can't connect to MySQL server: " . mysqli_error($conx) );
-	if(!$conx) {
+	if(!$connection) {
 		trigger_error('Could not connect to MySQL:' . mysqli_connect_error() );
 		// trigger_error uses our built in function in config.inc.php 
 	}
 	
 	// mysqli_select_db($conx, $db ) or die("Can't select that database: " . mysqli_error($conx) ); - doing in mysqli_connect
 	
-	return $conx;
+	return $connection;
 }
 
-function closeDB($conn) {
+function closeDB($connection) {
     $connection->close();
 }
 
