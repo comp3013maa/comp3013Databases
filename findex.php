86bd3800fac611e73afc1c00a860f4d9c3680061
abbasynia <manutdabbas1994@yahoo.co.uk>
2015-03-18 11:19:57 +0000

    Update findex.php

--- findex.php
+++ findex.php
@@ -1,7 +1,19 @@
 <?php
 //create_cat.php
 require 'fheader.php';
-include 'connect.php';
+
+$server = 'eu-cdbr-azure-west-b.cloudapp.net';
+$username = 'b6526a64c19791';
+$password = '5d020f59';
+$database = 'comp3013';
+if(!mysql_connect($server, $username, $password))
+{
+exit('Error: could not establish database connection');
+}
+if(!mysql_select_db($database)
+{
+exit('Error: could not select the database');
+}
 
 $sql = "SELECT
 			categories.cat_id,
