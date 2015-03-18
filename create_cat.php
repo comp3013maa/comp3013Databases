e76889ceed13a54281f5ce943ef4719a3d00f62d
abbasynia <abbasynia@Abbass-MacBook-Air.local>
2015-03-18 10:39:01 +0000

    Message

--- create_cat.php
+++ create_cat.php
@@ -3,48 +3,42 @@
 require 'fheader.php';
 include 'connect.php';
 
- 
-$sql = "SELECT
-            cat_id,
-            cat_name,
-            cat_description,
-        FROM
-            categories";
- 
-$result = mysql_query($sql);
- 
-if(!$result)
+echo '<h2>Create a category</h2>';
+if($_SESSION['signed_in'] == false | $_SESSION['user_level'] != 1 )
 {
-    echo 'The categories could not be displayed, please try again later.';
+	//the user is not an admin
+	echo 'Sorry, you do not have sufficient rights to access this page.';
 }
 else
 {
-    if(mysql_num_rows($result) == 0)
-    {
-        echo 'No categories defined yet.';
-    }
-    else
-    {
-        //prepare the table
-        echo '<table border="1">
-              <tr>
-                <th>Category</th>
-                <th>Last topic</th>
-              </tr>';
-             
-        while($row = mysql_fetch_assoc($result))
-        {              
-            echo '<tr>';
-                echo '<td class="leftpart">';
-                    echo '<h3><a href="category.php?id">' . $row['cat_name'] . '</a></h3>' . $row['cat_description'];
-                echo '</td>';
-                echo '<td class="rightpart">';
-                            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
-                echo '</td>';
-            echo '</tr>';
-        }
-    }
+	//the user has admin rights
+	if($_SERVER['REQUEST_METHOD'] != 'POST')
+	{
+		//the form hasn't been posted yet, display it
+		echo '<form method="post" action="">
+			Category name: <input type="text" name="cat_name" /><br />
+			Category description:<br /> <textarea name="cat_description" /></textarea><br /><br />
+			<input type="submit" value="Add category" />
+		 </form>';
+	}
+	else
+	{
+		//the form has been posted, so save it
+		$sql = "INSERT INTO categories(cat_name, cat_description)
+		   VALUES('" . mysql_real_escape_string($_POST['cat_name']) . "',
+				 '" . mysql_real_escape_string($_POST['cat_description']) . "')";
+		$result = mysql_query($sql);
+		if(!$result)
+		{
+			//something went wrong, display the error
+			echo 'Error' . mysql_error();
+		}
+		else
+		{
+			echo 'New category succesfully added.';
+		}
+	}
 }
- 
+
 require 'footer.php';
 ?>
