7f3e54165e95f1b46cb81fd0c4dee674597323bf
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-18 22:41:02 +0000

    Update report.php

--- report.php
+++ report.php
@@ -97,6 +97,8 @@ echo '
         </select>
     </div>
             <div class="form-group">
-            	<input type = "hidden" name = "submissionID" value = '.  $row['submissionID'] .wer vtA
+            <!?/.--	<input type = "hidden" name = "submissionID" value = ' . $row['submissionID'] . ' "/> --/?!>
+            	<p><input type="submit" value="Submit review"></p>
+             </div>
 require "footer.php";
 ?>
