38a56c19289f365f7dda9e3ab77995bd5a2898a9
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-18 18:32:54 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -36,102 +36,85 @@ if (isset($_GET['allocateGroups'])) {
 	// list of each group, and the ones they're assigned too 
 ?> 	
 
-<h3> Group Allocations </h3> <br /> 
-<h5> List of current allocations: </h5>	
-<div class="well">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>GroupID</th>
-          <th>Assigned 1</th>
-          <th>Assigned 2</th>
-          <th>Assigned 3</th>
-          <th style="width: 36px;"></th>
-        </tr>
-      </thead>
-      <tbody>
+	<h3> Group Allocations </h3> <br /> 
+	<h5> List of current allocations: </h5>	
+	<div class="well">
+	    <table class="table">
+	      <thead>
+	        <tr>
+	          <th>GroupID</th>
+	          <th>Assigned 1</th>
+	          <th>Assigned 2</th>
+	          <th>Assigned 3</th>
+	          <th style="width: 36px;"></th>
+	        </tr>
+	      </thead>
+	      <tbody>
+		<?php
+		$sql_model = new SQL_Model();
+		echo $sql_model->getGroupAllocations(); 
+	 // not closed as using again below	$sql_model->close();
+		?>	
+	      </tbody>
+	    </table>
+	</div>	
+
+	<!-- CREATE A NEW GROUP ALLOCATION-->
+	<hr /> 
+	<h3> Allocate a New Group: </h3>	
+	<p> Note: A maximum of 3 groups can be allocated for review to one group</p>
+
 	<?php
-	$sql_model = new SQL_Model();
-	echo $sql_model->getGroupAllocations(); 
- // not closed as using again below	$sql_model->close();
-	?>	
-      </tbody>
-    </table>
-</div>	
+	<?php
+	$groupList = array(); 
+	$groupList = $sql_model->getGroups();  
+	$sql_model->close(); ?>
 
-<!-- CREATE A NEW GROUP ALLOCATION-->
-<hr /> 
-<h3> Allocate a New Group: </h3>	
-<p> Note: A maximum of 3 groups can be allocated for review to one group</p>
+	<form class="form-horizontal" method="POST" action="admin.php?allocateGroups">
+	<fieldset>	
+		<div class="form-group">  
+		  <label class="col-md-4 control-label" for="textinput">Chose a group to assign: </label>  
+		  <div class="col-md-4">
+			<select name = "groupId" class="form-control input-md">
+				<?php
+				for ($i=0; $i < count($groupList); $i++ )  {						
+					echo "<option value =" . $groupList[$i] . ">" . $groupList[$i]. "</option>"; 	
+					}		
+				?>
+			</select>		
+		  </div>
+		</div>
 
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="textinput">Allocate To: </label>
+		  <div class="col-md-4">
+			<select name = "AllocateTo" class="form-control input-md">
+				<?php
+				for ($i=0; $i < count($groupList); $i++ )  {						
+					echo "<option value =" . $groupList[$i] . ">" . $groupList[$i]. "</option>"; 	
+					}		
+				?>
+			</select>	
+		  </div>
+		</div>
 
-<form class="form-horizontal" method="POST" action="users.php?add">
-<fieldset>	
-	<div class="form-group">
-	  <label class="col-md-4 control-label" for="textinput">Username: </label>  
-	  <div class="col-md-4">
-	  	<input id="textinput" name="username" type="text" class="form-control input-md">
-	  </div>
-	</div>
+		<!-- Button -->  
+		<div class="form-group">  
+				<label class="col-md-4 control-label" for="singlebutton"></label>
+				<div class="col-md-4">
+					<button type ="submit "id="singlebutton" name="register" class="btn btn-success"> Register </button>
+				</div>
+		</div>
+	</form>
 
-	<div class="form-group">
-	  <label class="col-md-4 control-label" for="textinput">Email: </label>
-	  <div class="col-md-4">
-	  	<input id="textinput" name="email" type="text" class="form-control input-md">
-	  </div>
-	</div>
-
-	<div class="form-group">
-			<label class="col-md-4 control-label" for="passwordinput">Password: </label>
-			<div class="col-md-4">
-	  			<input id="passwordinput" name="password1" type="password" class="form-control input-md">
-  	</div>
-	</div>
-
-	<div class="form-group">
-			<label class="col-md-4 control-label" for="passwordinput">Confirm Password: </label>
-			<div class="col-md-4">
-	  			<input id="passwordinput" name="password2" type="password" class="form-control input-md">
-  	</div>
-	</div>
-
-	<!-- Button -->
-	<div class="form-group">
-			<label class="col-md-4 control-label" for="singlebutton"></label>
-			<div class="col-md-4">
-				<button type ="submit "id="singlebutton" name="register" class="btn btn-success"> Register </button>
-			</div>
-	</div>
-</form>
-<?php
-$groupList = array(); 
-$groupList = $sql_model->getGroups();  
-$sql_model->close();
-
-	
-// var_dump($groupList); 
-
-echo '<select name = "groupId">';
-	for ($i=0; $i < count($groupList); $i++ )  {						
-
-		echo "<option value =" . $groupList[$i] . ">" . $groupList[$i]. "</option>"; 						
-echo '</select>'; 		
+	<?php
+	if(isset($_POST['newGroupAllocation'])) {
+			
 	}
 
-echo '<select name = "AllocateTo">';
-	for ($i=0; $i < count($groupList); $i++ )  {						
+}  
 
-		echo "<option value =" . $groupList[$i] . ">" . $groupList[$i]. "</option>"; 	
-		}					
-echo '</select>'; 	
-?> 
 
-<?php
-if(isset($_POST['newGroupAllocation'])) {
-	
-}
-
-}  // end allocateGroups if 
 
 if (isset($_GET['rankings'])) {
 
