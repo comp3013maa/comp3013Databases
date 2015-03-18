3b2dca974d1d50efad2159092e0a52601ab97bd2
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-18 01:26:39 +0000

    Update report.php

--- report.php
+++ report.php
@@ -35,7 +35,7 @@ $i = 0;
 //echo $report = $row['submissionName'];
 //echo $row
 echo file_get_contents($report[0]);
-echo file_get_contents($report[1]);echo file_get_contents($report[2]);
+echo file_get_contents($report[1]);echo file_get_contents($report[3]);
 date_default_timezone_set("Europe/London");
 $time = date("d/m/y h:ia");
 echo '
