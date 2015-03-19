c73c61b7ae239d6336a7ff7075822715dec3c760
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-19 23:39:01 +0000

    Update assessments.php

--- assessments.php
+++ assessments.php
@@ -1,7 +1,39 @@
 <?php
 require "header.php"; 
 
+$connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013')
+	 or die('Error1' . mysqli_error($connection));
+	 
+ $userID = $_SESSION['userID'];
+	 
+	 $query1 = "
+                SELECT groupID 
+                FROM users 
+          	WHERE userID=$userID
+	        ";
+	
+$result1 = mysqli_query($connection,$query1) or die('Error2' . mysqli_error($connection));
+$row1 = mysqli_fetch_assoc($result1);
+$groupID = $row1['groupID'];	 
 
+$query = "
+        SELECT grade, comments, byGroup
+        FROM grade
+        INNER JOIN groupassignments
+	ON grade.byGroup = groupassignments.groupID
+        WHERE groupassignments.assignedTo = $groupID
+          ";
+          
+$result = mysqli_query($connection,$query) or die('Error2' . mysqli_error($connection));
+        while($row = mysqli_fetch_assoc($result)){
+                echo $row['byGroup'];
+                echo $row['comments'];
+                echo $row['grade'];
+        }
+        
+          
+          
+mysqli_close($connection);
 
 require "footer.php";
 ?>
