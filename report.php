779bbb4e192a0eb07861fb7bbc1ce403af4987a9
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-19 02:50:54 +0000

    Update report.php

--- report.php
+++ report.php
@@ -63,11 +63,12 @@ $result = mysqli_query($connection,$query) or die('Error2' . mysqli_error($conne
         </ul>
 		</div>  ';
 		}
-		else{$query3= "
-INSERT INTO grade (submissionID, grade, comments, byGroup)
-VALUES ($_POST['submissionID'], $_POST['grade'], '$_POST['comments']', $groupID)
-";
-$result3 = mysqli_query($connection,$query3) or die('Error' . mysqli_error($connection));
+		else{
+			$query3= "
+				INSERT INTO grade (submissionID, grade, comments, byGroup)
+				VALUES ($_POST['submissionID'], $_POST['grade'], '$_POST['comments']', $groupID)
+				";
+$result3 = mysqli_query($connection,$query3) or die('Error4' . mysqli_error($connection));
 			echo '
 <div class="detailBox">
     <div class="titleBox">
