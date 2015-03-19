33c064f98785c6bc6f2e80a8c857113eb9cba506
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-19 21:31:05 +0000

    Update groupreports.php

--- groupreports.php
+++ groupreports.php
@@ -4,7 +4,7 @@ require "header.php";
 echo '
 	<ul> 
 		<li id = "indexList"> <a href = "report.php" class="listLinks"> Submit Gradings</a></li>                  		
-		<li id = "indexList"> <a href = "" class="listLinks">View Assessments</a></li>
+		<li id = "indexList"> <a href = "assessments.php" class="listLinks">View Assessments</a></li>
 		<li id = "indexList"> <a href = "" class="listLinks">View Rankings</a></li>
 	</ul>   
 	';
