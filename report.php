73e9fb6ddc8b482c9cb0a13312233ab6733002a2
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-19 03:42:16 +0000

    Update report.php

--- report.php
+++ report.php
@@ -67,11 +67,11 @@ $result = mysqli_query($connection,$query) or die('Error2' . mysqli_error($conne
 			
 			$subID = $_POST['submissionID'];
 			$grade = $_POST['grade'];
-			$comment = "bark";//$_POST['comments'];
+			$comment = $_POST['comments'];
 			
 			$query3 = "
 				INSERT INTO grade (submissionID, grade, comments, byGroup)
-				VALUES ($subID, $grade, '$comment', $groupID)
+				VALUES ($subID, $grade, '$comment' , $groupID)
 				";
 $result3 = mysqli_query($connection, $query3) or die('Error4' . mysqli_error($connection));
 			echo '
