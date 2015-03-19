cabdc29c8c591b9d61b91df377c44b8815170608
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-19 21:28:09 +0000

    Create groupreports.php

---
+++ groupreports.php
@@ -0,0 +1,13 @@
+<?php
+require "header.php"; 
+
+echo '
+	<ul> 
+		<li id = "indexList"> <a href = "report.php" class="listLinks"> Submit Gradings</a></li>                  		
+		<li id = "indexList"> <a href = "" class="listLinks">View Assessments</a></li>
+		<li id = "indexList"> <a href = "" class="listLinks">View Rankings</a></li>
+	</ul>   
+	';
+
+require "footer.php";
+?>
