86dd1e21a1c8ccc6946bce7e5bfd10eaec435d1b
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-20 20:45:15 +0000

    Update submitted.php

--- submitted.php
+++ submitted.php
@@ -1,6 +1,6 @@
 <?php
 require "header.php"; 
-echo '<h3>Upload your report</h3> <p></p>';
+echo '<h3>Upload your report</h3> <br />';
 echo 'Your group has already uploaded a report!';
 require "footer.php";
 ?>
