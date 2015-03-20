0524e8b2e5f5e97c7f2ae88fe5bcdf0a43992937
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-20 20:38:42 +0000

    Update groupreports.php

--- groupreports.php
+++ groupreports.php
@@ -2,12 +2,19 @@
 require "header.php"; 
 
 echo '
-	<ul> 
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+              	<h3 class="panel-title">ADMIN</h3>
+            </div>
+            <div class="panel-body">
+                    <ul> 
 		<li id = "indexList"> <a href = "report.php" class="listLinks"> Submit Gradings</a></li>                  		
 		<li id = "indexList"> <a href = "assessments.php" class="listLinks">View Assessments</a></li>
-		<li id = "indexList"> <a href = "" class="listLinks">View Rankings</a></li>
-	</ul>   
-	';
+		<li id = "indexList"> <a href = "" class="listLinks">View Rankings</a></li>         
+		 </ul>   
+            </div>
+        </div>  
+    </div>  ';
 
 require "footer.php";
 ?>
