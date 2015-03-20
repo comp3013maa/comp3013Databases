1eb722f5fc44998e0ced9ffd5cf82252a939b7ee
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-20 02:23:05 +0000

    Update unauthorised.php

--- unauthorised.php
+++ unauthorised.php
@@ -1,17 +1,18 @@
 <?php
 require "header.php";
+require_once 'include/helper_functions.php';
 
 if (!isset($_SESSION['userID'])) {
-  echo '<b> You are not logged in </b> </br>';
-  echo 'Please <a href="login.php"> log in </a>';
-
+  echo errorMessage("You are not logged in", 'Please <a href="login.php"> log in </a>');	
 }
 
-if (isset($_SESSION['userID'])) {
-  echo '<b> You are already logged in </b> </br>';
-  echo 'Go back to <a href="index.php"> Home </a>';  
+if (isset($_SESSION['userID']) && isset($_GET['logged_in'])) {
+  echo errorMessage('You are already logged in', 'Go back to <a href="index.php"> Home </a>');
 }
 
+if(!isset($_SESSION['admin']) && isset($_GET['admin'])) {
+	echo errorMessage("Unauthorised Access", 'You cannot access admin only pages. For any issues please <a href="contact.php">contact us</a>.');
+}	
 
 require "footer.php";
 ?>
