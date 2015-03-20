b7cf55707be817d0c9c519e2cbe6fd5fd41fa317
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-20 00:09:57 +0000

    Update assessments.php

--- assessments.php
+++ assessments.php
@@ -7,22 +7,9 @@ if(!isset($_SESSION['userID'])){
 
 $connection = mysqli_connect('eu-cdbr-azure-west-b.cloudapp.net','b6526a64c19791','5d020f59','comp3013')
 	 or die('Error1' . mysqli_error($connection));
-
-	$query2 = " 
-		SELECT groupID
-		FROM groupassignments
-		WHERE assignedTo = $groupID
-		";
-	
-$result2 = mysqli_query($connection,$query2) or die('Error' . mysqli_error($connection));
-
+	 
 echo '<h3>Submit Gradings</h3>';
 
-echo 'Your group is assigned to review groups ';
-
-while($row2 = mysqli_fetch_assoc($result2)){
-	 $row2['groupID']. ' ';
-}
 	 
  $userID = $_SESSION['userID'];
 	 
@@ -50,6 +37,22 @@ $result = mysqli_query($connection,$query) or die('Error2' . mysqli_error($conne
                 echo $row['comments'];
                 echo $row['grade'];
         }
+  	
+  		$query2 = " 
+		SELECT groupID
+		FROM groupassignments
+		WHERE assignedTo = $groupID
+		";
+	
+$result2 = mysqli_query($connection,$query2) or die('Error' . mysqli_error($connection));
+
+
+
+echo 'Your group is assigned to review groups ';
+
+while($row2 = mysqli_fetch_assoc($result2)){
+	 $row2['groupID']. ' ';
+}
   	
 mysqli_close($connection);
 
