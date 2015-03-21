d574914904aa6336bddf735cd3a92d300c7b1903
abbasynia <manutdabbas1994@yahoo.co.uk>
2015-03-21 10:42:08 +0000

    Update create_cat.php

--- create_cat.php
+++ create_cat.php
@@ -6,7 +6,7 @@ include 'fheader.php';
 include 'connect.php';
 
 echo '<h2>Create a category</h2>';
-if($_SESSION['signed_in'] == false | $_SESSION['user_level'] != 1 )
+if(!isset ($_SESSION['userID']))
 {
 	//the user is not an admin
 	echo 'Sorry, you do not have sufficient rights to access this page.';
