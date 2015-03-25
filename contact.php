d5fc2e2af4aac115845b35e5136b7566af62a039
MahiKhan <mahi.u.khan@googlemail.com>
2015-03-25 22:23:01 +0000

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,6 +5,9 @@ require "header.php";
 <h3>Contact Us </h3> <br /> 
 <p> Please Contact us at: abbuz (at) hotmail (dot) com </p>
 
+<p> Alternatively please post in our <a href="forum.php">forum</a>. </p>
+
+
 <?php
 require "footer.php";
 ?>
