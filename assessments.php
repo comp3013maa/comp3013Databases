de975927634601eb456594657d38b62ac467d20f
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-25 21:42:18 +0000

    Update assessments.php

--- assessments.php
+++ assessments.php
@@ -71,7 +71,7 @@ echo "Your group's assessments are shown below.". "<p></p>";
         			      	<div class="commentText">
         		        		<p class="">' . $row['comments']. '</p> 
              					<p class="">Grade: ' . $row['grade']. '/10</p>
-             					<p class="">Group average mark: ' . $row3['averageMark']. '/10</p>
+             					
                 			</div>
         				</li>
 				 </ul>
