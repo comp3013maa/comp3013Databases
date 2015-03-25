e36af2626c2fe15e35741eb7a269fbc545eaba8e
abbasynia <manutdabbas1994@yahoo.co.uk>
2015-03-25 14:12:43 +0000

    Update findex.php

--- findex.php
+++ findex.php
@@ -7,7 +7,7 @@ echo '<form  method="post" action="search.php?go"  id="searchform">
 	      <input  type="submit" name="submit" value="Search"> 
 	    </form>';
 	    
-	    if(isset($_POST['submit'])){ 
+	    if(isset($_POST['submit']) ){ 
 	    	if(isset($_GET['go'])){ 
 	  	$sql = "select * from posts where text LIKE *". $_GET['go']."*";
 	  	$result = mysqli_query($conn,$sql) or die('Error2' . mysqli_error($conn)); 
@@ -24,8 +24,9 @@ echo '<form  method="post" action="search.php?go"  id="searchform">
 			echo '<h3><a href="topic.php?id=' . $row['post_topic'] . '">' . $row['post_content'] . '</a></h3>' . $row['post_id'];
 			echo '</td>';
 			echo '<td class="rightpart">';
-	    	}
-	  } 
+	    		}
+	  	} 
+	 }
 
 require "footer.php";
 ?>
