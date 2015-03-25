2d6a3ef80124834d9f3d98620f63a312d7951aa9
abbasynia <manutdabbas1994@yahoo.co.uk>
2015-03-25 11:22:33 +0000

    Update topic.php

--- topic.php
+++ topic.php
@@ -69,7 +69,7 @@ else
 				}
 			}
 			
-			if(!isset ($_SESSION['userID'] )
+			if(!isset($_SESSION['userID']) )
 			{
 				echo '<tr><td colspan=2>You must be <a href="signin.php">signed in</a> to reply. You can also <a href="signup.php">sign up</a> for an account.';
 			}
