de2975b5572a0650a7f260e8bf685936b6c3e431
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2015-03-25 16:40:10 +0000

    Update uploadxml.php

--- uploadxml.php
+++ uploadxml.php
@@ -31,7 +31,7 @@ if(isset($_POST['uploaded'])) {
 	
 $file = $_FILES['file']['name'];	
 $directory = 'uploads/'. basename($file);
-$xml = simplexml_load_string(file_get_contents($_FILES['tmp_name'])) or die("Error: Cannot create object");
+$xml = simplexml_load_string(file_get_contents($_FILES['file']['tmp_name'])) or die("Error: Cannot create object");
 $validUpload = true;
 $extension = pathinfo($directory,PATHINFO_EXTENSION);
 $marker = 0;
