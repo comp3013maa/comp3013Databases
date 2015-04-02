20922bdffc93bdea32720d22b9a27c49c6b5e79f
abbasynia <manutdabbas1994@yahoo.co.uk>
2015-04-02 13:14:56 +0100

    Update fheader.php

--- fheader.php
+++ fheader.php
@@ -5,7 +5,7 @@ header ("Content-type: image/png");
 $rno = rand(1000,99999);
 $_SESSION['ckey'] = md5($rno);
 
-$img_handle = imageCreateFromPNG("bg1.PNG");
+$img_handle = imageCreateFromPNG("http://s18.postimg.org/ov6ea0t11/bg1.png");
 $color = ImageColorAllocate ($img_handle, 0, 0, 0);
 ImageString ($img_handle, 5, 20, 13, $rno, $color);
 ImagePng ($img_handle);
